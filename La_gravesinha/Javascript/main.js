a45161597e1d24b83b709af1838fb30cb1fe2147
Janzt <77674605+Janzt@users.noreply.github.com>
2021-03-16 02:18:17 +0400

    Delete La_gravesinha directory

--- La_gravesinha/Javascript/main.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelector('.menu-btn').addEventListener('click',() =>{
-     document.querySelector('.nav-menu-center').classList.toggle('show');
-});
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.contenido-principal', { delay:500});
-ScrollReveal().reveal('.banner-one', { delay:500});
-ScrollReveal().reveal('.banner-two', { delay:500});
